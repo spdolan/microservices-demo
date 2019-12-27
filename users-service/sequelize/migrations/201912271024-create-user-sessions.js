1dde7fe12333884bbcbe09e40840ae2f3250e011
Sean Dolan <sdolan@theclimateservice.com>
2019-12-27 09:29:32 -0600

    added sequelize from part 2, created migrations files and added migrate scripts to package.json for both listings and users.

---
+++ users-service/sequelize/migrations/201912271024-create-user-sessions.js
@@ -0,0 +1,32 @@
+module.exports.up = (queryInterface, DataTypes) => {
+    return queryInterface.createTable("userSessions", {
+        id: {
+            allowNull: false,
+            primaryKey: true,
+            type: DataTypes.UUID
+        },
+        userId: {
+            allowNull: false,
+            references: {
+                key: "id",
+                model: "users"
+            },
+            type: DataTypes.ID
+        },
+        expiresAt: {
+            allowNull: false,
+            type: DataTypes.DATE
+        },
+        createdAt: {
+            allowNull: false,
+            type: DataTypes.DATE
+        }
+    },
+    {
+        charset: "utf8"
+    }
+
+    )
+};
+
+module.exports.down = queryInterface => queryInterface.dropTable("userSessions");
